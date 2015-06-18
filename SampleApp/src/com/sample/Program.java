4d2152c68a2311b3afb512f548b6657b3bea08b9
Prakash Kumar <Kumar_Prakash@hcl.com>
2015-06-18 00:52:37 -0700

    Added StringUtils

--- SampleApp/src/com/sample/Program.java
+++ SampleApp/src/com/sample/Program.java
@@ -1,10 +1,12 @@
 package com.sample;
 
+import com.sample.utils.StringUtils;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello Prakash");
+		String input = "Prakash";
+		System.out.printf("All chars in %s are %s.", input, StringUtils.HasUniqueCharacters(input) ? "unique" : "not unique");
 	}
 
 }
