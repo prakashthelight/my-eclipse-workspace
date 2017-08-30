caa97cc755799067f8ca1e36d083ee53741dae39
Kumar,Prakash <prakash.thelight@gmail.com>
2017-08-30 00:23:18 -0700

    LeetCode 012 - Integer to Roman

---
+++ console-app/src/com/samples/leetcode/LeetCode012.java
@@ -0,0 +1,31 @@
+package com.samples.leetcode;
+
+/**
+ * Integer to Roman
+ * https://leetcode.com/problems/integer-to-roman
+ * @author Kumar, Prakash
+ *
+ */
+public class LeetCode012 {
+
+	public static void main(String[] args) {
+		int num = 3999;
+		
+		System.out.println(intToRoman(num));
+	}
+
+	/**
+	 * convert number to roman
+	 * @param num
+	 * @return
+	 */
+	public static String intToRoman(int num) {
+
+		String M[] = { "", "M", "MM", "MMM" };
+		String C[] = { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+		String X[] = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+		String I[] = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+
+		return M[num / 1000] + C[(num % 1000) / 100] + X[(num % 100) / 10] + I[num % 10];		
+	}
+}
