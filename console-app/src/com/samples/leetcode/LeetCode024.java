8cfacfbb21f6e0514cf41aa5583ac8b61fc3014d
Kumar,Prakash <prakash.thelight@gmail.com>
2017-11-07 10:30:44 -0800

    LeetCode 024 - Swap Nodes in Pairs

---
+++ console-app/src/com/samples/leetcode/LeetCode024.java
@@ -0,0 +1,48 @@
+package com.samples.leetcode;
+
+import com.samples.ds.utils.MyLinkedListUtil;
+import com.samples.model.ListNode;
+
+/**
+ * Swap Nodes in Pairs <br/>
+ * https://leetcode.com/problems/swap-nodes-in-pairs
+ * 
+ * @author Kumar, Prakash
+ *
+ */
+public class LeetCode024 {
+
+	public static void main(String[] args) {
+		ListNode head = MyLinkedListUtil.createLinkedList(new int[] {1,2,3,4,5,6});
+		
+		head = swapPairs(head);
+		
+		MyLinkedListUtil.print(head);
+	}
+
+	/**
+	 * Swaps pair of LinkedList nodes
+	 * @param head
+	 * @return
+	 */
+	public static ListNode swapPairs(ListNode head) {
+		if (head == null || head.next == null)
+			return head;
+
+		ListNode p1 = head;
+		ListNode p2 = head.next;	
+
+		while (p1 != null && p2 != null) {
+			int temp = p1.value;
+			p1.value = p2.value;
+			p2.value = temp;
+
+			p1 = p2.next;
+
+			if (p1 != null)
+				p2 = p1.next;
+		}
+
+		return head;
+	}
+}
